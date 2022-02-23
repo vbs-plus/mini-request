4a4f84a37314236d13abc212850752ff16b85aa5
zz <lzheng.zj@gmail.com>
2022-02-23 16:59:20 +0800

    ✨ feat(network-utils): common utils and polyfill

---
+++ network-utils/src/promise.finally.ts
@@ -0,0 +1,27 @@
+/// <reference lib="es2015.promise"/>
+/// <reference lib="es2018.promise"/>
+interface Promise<T> {
+  // tslint:disable-next-line: no-reserved-keywords
+  readonly constructor: PromiseConstructor;
+}
+
+if (!Promise.prototype.finally) {
+  Promise.prototype.finally = function <T>(
+    this: Promise<T>,
+    onfinally?: (() => void) | undefined | null
+  ): Promise<T> {
+    if (onfinally) {
+      const P = this.constructor;
+      // tslint:disable
+      return this.then(
+        (value) => P.resolve(onfinally()).then(() => value),
+        (reason) =>
+          P.resolve(onfinally()).then(() => {
+            throw reason;
+          })
+      );
+    } else {
+      return this;
+    }
+  };
+}
